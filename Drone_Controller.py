7990c5f164a18de18a168a0d46222eb53604ce04
benj4865 <bena380dk@gmail.com>
2025-12-17 23:12:02 +0100

    Added proper instance handling of the drone object instad of modifying the class variables. Changes way that object is handled and opens door to multiple drones in a simulation

--- Drone_Controller.py
+++ Drone_Controller.py
@@ -1,17 +1,18 @@
 # This program is to simulate the drone controller, so it can be isolated from the simulation
-# and it can be used for later implementation in a real drone.
 
 class Drone_Controller:
-
-    velocity = 0
-    bearing = 0
-    altitude = 0
-    position = (0,0)
-    flight_time = 0
-    battery_full_capacity = 100
-    battery_Wh_left = 100 # in watt hours
-    discharge_rate = 200 #in watt
-    distance_flown = 0
-
-    #Static Variable
+    #Static Variables
     drone_base = (55.607124, 12.393114)
+
+    battery_full_capacity = 100
+    discharge_rate = 200 # in watt
+
+    def __init__(self):
+        # Variables kept at a pr. instance level
+        self.velocity = 0
+        self.bearing = 0
+        self.altitude = 0
+        self.position = (0,0)
+        self.flight_time = 0
+        self.battery_Wh_left = 100 # in watt hours
+        self.distance_flown = 0
